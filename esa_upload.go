1e6a3a874494c8f9d2b91ad88786d71c10673f9e
uobikiemukot <uobikiemukot@gmail.com>
2018-05-16 11:50:18 +0900

    feature: implement file uploading command, esa_upload

---
+++ esa_upload.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/uobikiemukot/go-esa/esa"
+)
+
+func main() {
+	apikey := os.Getenv("ESA_API_KEY")
+	team   := os.Getenv("ESA_TEAM")
+	client := esa.NewClient(apikey)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: ./esa_upload ImageFile")
+		os.Exit(1)
+	}
+
+	url, err := client.Attachment.UploadAttachmentFile(team, os.Args[1])
+	if err != nil {
+		fmt.Printf("UploadAttachmentFile Failed: %v\n", err)
+		os.Exit(2)
+	}
+	fmt.Println(url)
+	os.Exit(0)
+}
